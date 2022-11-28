492cbf681a502b5d0385fb191b05d669ff94951a
ant <theant@mail.ru>
2022-11-28 22:41:58 +0400

    добавил редактирумое, перетаскиваемое поле

--- app/src/main/java/com/tests/vicuesofttest/ui/PlayerFragment.kt
+++ app/src/main/java/com/tests/vicuesofttest/ui/PlayerFragment.kt
@@ -1,8 +1,13 @@
 package com.tests.vicuesofttest.ui
 
+import android.content.ClipData
+import android.content.ClipDescription
+import android.net.Uri
 import android.os.Bundle
+import android.view.DragEvent
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.DragShadowBuilder
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -35,10 +40,61 @@ class PlayerFragment : Fragment() {
         val videoPath =
             "https://storage.googleapis.com/assets-stage-bgrem-deelvin-com/bg/videos/nature_3.mp4"
 
-        videoView.setVideoPath(videoPath)
+        videoView.setVideoURI(Uri.parse(videoPath))
         videoView.start()
-
         videoView.setOnCompletionListener { videoView.resume() }
+
+        val editText = binding.editThisTextFieldFragmentPlay
+        binding.BtnFragmentPlayAddText.setOnClickListener {
+            if (editText.visibility == View.INVISIBLE)
+                editText.visibility = View.VISIBLE
+        }
+
+        editText.setOnLongClickListener {
+            val clipText: String = editText.text.toString()
+            val clipItem = ClipData.Item(clipText)
+            val mimeType = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
+            val data = ClipData(clipText, mimeType, clipItem)
+
+            val dragShadowBuilder = DragShadowBuilder(it)
+            it.startDragAndDrop(data, dragShadowBuilder, it, 0)
+
+            it.visibility = View.INVISIBLE
+            true
+        }
+
+        val dragListener = View.OnDragListener { dragView, dragEvent ->
+            when (dragEvent.action) {
+                DragEvent.ACTION_DRAG_STARTED -> {
+                    dragEvent.clipDescription.hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)
+                }
+                DragEvent.ACTION_DRAG_ENTERED -> {
+                    dragView.invalidate()
+                    true
+                }
+                DragEvent.ACTION_DRAG_LOCATION -> {
+                    true
+                }
+                DragEvent.ACTION_DRAG_EXITED -> {
+                    dragView.invalidate()
+                    true
+                }
+                DragEvent.ACTION_DROP -> {
+                    val newView = dragEvent.localState as View
+                    newView.translationX = dragEvent.x
+                    newView.translationY = dragEvent.y
+                    newView.visibility = View.VISIBLE
+                    true
+                }
+
+                DragEvent.ACTION_DRAG_ENDED -> {
+                    dragView.invalidate()
+                    true
+                }
+                else -> false
+            }
+        }
+        binding.VideoViewContainerFragmentPlay.setOnDragListener(dragListener)
     }
 
     override fun onDestroyView() {
